b835f61af1b80dba193e2c123a56e06032018c53
mikachu-chu <mikka.abarra@gmail.com>
2026-01-14 19:20:21 +0800

    Update visualSig.py

--- visualSig.py
+++ visualSig.py
@@ -1,10 +1,10 @@
 import numpy as np
 
-rgb_train = np.load("./res/nsl/RGB_TRAIN.npz")["patches"]
-rgb_test = np.load("./res/nsl/RGB_TEST.npz")["patches"]
+rgb_train = np.load("./res/unsw/RGB_TRAIN.npz")["patches"]
+rgb_test = np.load("./res/unsw/RGB_TEST.npz")["patches"]
 
-att_train = np.load("./res/nsl/IM.npz")["patches"]
-att_test = np.load("./res/nsl/IM_TEST.npz")["patches"]
+att_train = np.load("./res/unsw/IM.npz")["patches"]
+att_test = np.load("./res/unsw/IM_TEST.npz")["patches"]
 
 #CONVERT DTYPE TO NUMPY FLOAT32 TO COMPUTE INERTIA VIA NUMPY
 att_train = att_train.astype(np.float32)
@@ -16,4 +16,5 @@ print("ATT_TRAIN:", att_train.shape, att_train.dtype)
 print("ATT_TEST:", att_test.shape, att_test.dtype)
 print("RGB_TRAIN:", rgb_train.shape, rgb_train.dtype)
 print("RGB_TEST:", rgb_test.shape, rgb_test.dtype)
-
+print(np.load("./res/unsw/RGB_TRAIN.npz")["labels"].shape)
+print(np.load("./res/unsw/RGB_TEST.npz")["labels"].shape)
