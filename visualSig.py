24f02ee287033da2d23efd32d6257d06486d3bf7
Dolly-Chaerry <douleiamaeyap@gmail.com>
2026-01-14 18:04:17 +0800

    Create visualSig.py

---
+++ visualSig.py
@@ -0,0 +1,19 @@
+import numpy as np
+
+rgb_train = np.load("./res/nsl/RGB_TRAIN.npz")["patches"]
+rgb_test = np.load("./res/nsl/RGB_TEST.npz")["patches"]
+
+att_train = np.load("./res/nsl/IM.npz")["patches"]
+att_test = np.load("./res/nsl/IM_TEST.npz")["patches"]
+
+#CONVERT DTYPE TO NUMPY FLOAT32 TO COMPUTE INERTIA VIA NUMPY
+att_train = att_train.astype(np.float32)
+att_test = att_test.astype(np.float32)          
+rgb_train = rgb_train.astype(np.float32)
+rgb_test = rgb_test.astype(np.float32)
+
+print("ATT_TRAIN:", att_train.shape, att_train.dtype)
+print("ATT_TEST:", att_test.shape, att_test.dtype)
+print("RGB_TRAIN:", rgb_train.shape, rgb_train.dtype)
+print("RGB_TEST:", rgb_test.shape, rgb_test.dtype)
+
