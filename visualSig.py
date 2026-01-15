fa3058c47516d323fac718a620133fd4ad3b3931
mikachu-chu <mikka.abarra@gmail.com>
2026-01-15 08:49:09 +0800

    Update visualSig.py

--- visualSig.py
+++ visualSig.py
@@ -1,10 +1,14 @@
 import numpy as np
+import matplotlib.pyplot as plt
 
-rgb_train = np.load("./res/unsw/RGB_TRAIN.npz")["patches"]
-rgb_test = np.load("./res/unsw/RGB_TEST.npz")["patches"]
+dataset = "nsl"
 
-att_train = np.load("./res/unsw/IM.npz")["patches"]
-att_test = np.load("./res/unsw/IM_TEST.npz")["patches"]
+rgb_train = np.load(f"./res/{dataset}/RGB_TRAIN.npz")["patches"]
+rgb_test = np.load(f"./res/{dataset}/RGB_TEST.npz")["patches"]
+att_train = np.load(f"./res/{dataset}/ATT_TRAIN.npz")["patches"]
+att_test = np.load(f"./res/{dataset}/ATT_TEST.npz")["patches"]
+y_train = np.load(f"./res/{dataset}/RGB_TRAIN.npz")["labels"]
+y_test = np.load(f"./res/{dataset}/RGB_TEST.npz")["labels"]
 
 #CONVERT DTYPE TO NUMPY FLOAT32 TO COMPUTE INERTIA VIA NUMPY
 att_train = att_train.astype(np.float32)
@@ -16,5 +20,82 @@ print("ATT_TRAIN:", att_train.shape, att_train.dtype)
 print("ATT_TEST:", att_test.shape, att_test.dtype)
 print("RGB_TRAIN:", rgb_train.shape, rgb_train.dtype)
 print("RGB_TEST:", rgb_test.shape, rgb_test.dtype)
-print(np.load("./res/unsw/RGB_TRAIN.npz")["labels"].shape)
-print(np.load("./res/unsw/RGB_TEST.npz")["labels"].shape)
+
+sig_orig = {}
+sig_att = {}
+
+for c in np.unique(y_test):    #list of all images per class c
+    sig_att[c] = att_test[y_test == c].mean(axis=0)  # already (12,12,3)
+    sig_orig[c] = rgb_test[y_test == c].mean(axis=0)
+    plt.imsave(f"visualSig_{c}.png", sig_att[c].astype(np.uint8))
+
+if dataset == "maldroid":
+    classes = ["Benign", "Adware", "Banking", "SMS", "Ransomware"]
+elif dataset == "nsl":
+    classes = ["Benign", "DoS", "Probe", "R2L", "U2R"]
+n = len(classes)
+
+fig, axes = plt.subplots(1, n, figsize=(4*n, 4))
+
+for ax, c, label in zip(axes, sig_att.keys(), classes):
+    im = ax.imshow(sig_att[c].astype(np.uint8))
+    ax.set_title(f"{label}")
+    ax.axis("off")
+
+fig.colorbar(im, ax=axes, fraction=0.02)
+plt.savefig("attention-signatures.png", bbox_inches="tight")
+
+def avg_inertia(X, mu):
+    diffs = X - mu
+    diffs = diffs.reshape(len(X), -1)
+    return np.mean(diffs**2)
+
+inertia_orig = {}
+inertia_att  = {}
+
+for c in np.unique(y_test):
+    inertia_orig[c] = avg_inertia(rgb_test[y_test == c], sig_orig[c])
+    inertia_att[c]  = avg_inertia(att_test[y_test == c], sig_att[c])
+
+print(inertia_orig)
+print(inertia_att)
+
+
+avg_distance_maps = {}
+
+for c, mu_c in sig_att.items():
+    distance_sum = np.zeros(mu_c.shape[:2])  # (H, W)
+    count = 0
+
+    for c2, mu_c2 in sig_att.items():
+        if c2 == c:
+            continue
+
+        diff = mu_c - mu_c2                   # (H, W, 3)
+        dist = np.linalg.norm(diff, axis=-1)  # (H, W)
+
+        distance_sum += dist
+        count += 1
+
+    avg_distance_maps[c] = distance_sum / count
+
+all_maps = np.stack(list(avg_distance_maps.values()))  # (C, H, W)
+
+global_min = 0
+global_max = all_maps.max()
+
+classes = list(avg_distance_maps.keys())
+C = len(classes)
+
+fig, axes = plt.subplots(1, C, figsize=(4*C, 4))
+
+for ax, c in zip(axes, classes):
+    im = ax.imshow(avg_distance_maps[c],
+                   cmap="RdPu",
+                   vmin=0,
+                   vmax=global_max)
+    ax.set_title(f"Class {c}")
+    ax.axis("off")
+
+fig.colorbar(im, ax=axes, fraction=0.02)
+plt.savefig("avg_distance_all_classes.png", bbox_inches="tight")
